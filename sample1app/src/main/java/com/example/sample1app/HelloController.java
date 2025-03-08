9a19bbab8248aefcc4e1d6780c98dfec19620330
caleressmisu <missun1201@me.com>
2025-03-08 14:54:08 +0900

    2-20

---
+++ sample1app/src/main/java/com/example/sample1app/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.sample1app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/{temp}")
+    public String index(@PathVariable("temp") String temp) {
+        switch (temp) {
+            case "index":
+                return "index";
+            default:
+                return "other";
+        }
+    }
+
+}
