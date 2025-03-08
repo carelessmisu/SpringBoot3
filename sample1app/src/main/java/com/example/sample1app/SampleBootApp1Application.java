c2a6356fbfe3bea0f38eeca4ac08de018dee0c20
caleressmisu <missun1201@me.com>
2025-03-08 14:27:57 +0900

    2-13

--- sample1app/src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ sample1app/src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -1,81 +1,38 @@
 package com.example.sample1app;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SampleBootApp1Application {
-
-	private final DataObject[] data = {
-			new DataObject("noname", "no email address", 0),
-			new DataObject("taro", "taro@yamada", 39),
-			new DataObject("hanako", "hanako@flower", 28),
-			new DataObject("sachiko", "sachiko@happy", 17),
-			new DataObject("jiro", "jiro@change", 6)
-	};
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleBootApp1Application.class, args);
 
 	}
 
-	@RequestMapping("/{num}")
-	public DataObject index(@PathVariable("num") int num) {
-		int n = num < 0 ? 0 : num >= data.length ? 0 : num;
-		return data[n];
+	@RequestMapping("/")
+	public String index(
+			HttpServletRequest request,
+			HttpServletResponse response) {
+		response.setContentType(MediaType.TEXT_HTML_VALUE);
+		String content = """
+				<html>
+				  <head>
+				  <title>Sample App</title>
+				  </head>
+				  <body>
+				  <h1>Sample App</h1>
+				  <p>This is sample app page!</p>
+				</html>
+				""";
+		return content;
 	}
 }
-
-// Lombok を使って簡潔に記述
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-class DataObject {
-	private String name;
-	private String mail;
-	private int age;
-}
-
-// class DataObject {
-// 	private String name;
-// 	private String mail;
-// 	private int age;
-
-// 	public DataObject(String name, String mail, int age) {
-// 		super();
-// 		this.name = name;
-// 		this.mail = mail;
-// 		this.age = age;
-// 	}
-
-// 	public String getName() {
-// 		return name;
-// 	}
-
-// 	public void setName(String name) {
-// 		this.name = name;
-// 	}
-
-// 	public String getMail() {
-// 		return mail;
-// 	}
-
-// 	public void setMail(String mail) {
-// 		this.mail = mail;
-// 	}
-
-// 	public int getAge() {
-// 		return age;
-// 	}
-
-// 	public void setAge(int age) {
-// 		this.age = age;
-// 	}
-// }
