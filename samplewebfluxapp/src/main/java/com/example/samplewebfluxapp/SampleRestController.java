24d5c6b1f5782bfe8714e76fda0ff244fc0ed4c0
caleressmisu <missun1201@me.com>
2025-03-09 19:17:40 +0900

    add Spring WebFlux

---
+++ samplewebfluxapp/src/main/java/com/example/samplewebfluxapp/SampleRestController.java
@@ -0,0 +1,27 @@
+package com.example.samplewebfluxapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class SampleRestController {
+
+	@RequestMapping("/flux")
+	public Mono<String> flux() {
+		return Mono.just("Hello Flux(Mono).");
+	}
+
+	@RequestMapping("/flux2")
+	public Flux<String> flux2() {
+		return Flux.just("Hello Flux.", "これはFluxのサンプルです。");
+	}
+
+	@RequestMapping("/post")
+	public Mono<Post> post() {
+		Post post = new Post(0, 0, "dummy", "dummy message...");
+		return Mono.just(post);
+	}
+}
