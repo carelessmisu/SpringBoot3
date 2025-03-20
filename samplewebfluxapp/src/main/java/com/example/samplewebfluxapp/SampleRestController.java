436eb402ac1572c19f3312b06c182601e4c4a45f
caleressmisu <missun1201@me.com>
2025-03-20 15:34:47 +0900

    6-2

--- samplewebfluxapp/src/main/java/com/example/samplewebfluxapp/SampleRestController.java
+++ samplewebfluxapp/src/main/java/com/example/samplewebfluxapp/SampleRestController.java
@@ -3,17 +3,60 @@ package com.example.samplewebfluxapp;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import jakarta.annotation.PostConstruct;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
 
 @RestController
 public class SampleRestController {
+
+  private final WebClient webClient;
+
+  public SampleRestController(WebClient.Builder builder) {
+    super();
+    webClient = builder.baseUrl("jsonplaceholder.typicode.com").build();
+  }
+
+  @RequestMapping("/web/{id}")
+  public Mono<Post> web(@PathVariable("id") int id) {
+    return this.webClient.get()
+        .uri("/posts/" + id)
+        .accept(MediaType.APPLICATION_JSON)
+        .retrieve()
+        .bodyToMono(Post.class);
+  }
+
+  @RequestMapping("/web")
+  public Flux<Post> web2() {
+    return this.webClient.get()
+        .uri("/posts")
+        .accept(MediaType.APPLICATION_JSON)
+        .retrieve()
+        .bodyToFlux(Post.class);
+  }
+
+  @RequestMapping("/webpost/{id}")
+  public Mono<Post> web3(@PathVariable("id") int id) {
+    Post post = postRepository.findById(id);
+    return this.webClient.post()
+        .uri("/posts")
+        .accept(MediaType.APPLICATION_JSON)
+        .bodyValue(post)
+        .retrieve()
+        .bodyToMono(Post.class);
+  }
 
   @Autowired
   private PostRepository postRepository;
@@ -77,5 +120,23 @@ public class SampleRestController {
   public Flux<Object> posts() {
     List<Post> posts = postRepository.findAll();
     return Flux.fromArray(posts.toArray());
+  }
+
+  @RequestMapping("/file")
+  public Mono<String> file() {
+    String result = "";
+    try {
+      ClassPathResource cr = new ClassPathResource("sample.txt");
+      InputStream is = cr.getInputStream();
+      InputStreamReader isr = new InputStreamReader(is, "utf-8");
+      BufferedReader br = new BufferedReader(isr);
+      String line;
+      while ((line = br.readLine()) != null) {
+        result += line;
+      }
+    } catch (IOException e) {
+      result = e.getMessage();
+    }
+    return Mono.just(result);
   }
 }
